757a411b9caf3583c05e97e1f6c224cf79d90244
“NicolasAT08” <nicolasaarangurent@gmail.com>
2021-12-07 17:14:42 -0500

    Some changes final

--- bank_auth_example/urls.py
+++ bank_auth_example/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from auth_example import views
+from auth_example.views.verifyTokenView import VerifyTokenView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
-    path('verifyToken/', views.VerifyTokenView.as_view()),
+    path('verifyToken/', views.VerifyTokenView.as_view()), 
     path('user/', views.UserCreateView.as_view()),
     path('user/<int:pk>/', views.UserDetailView.as_view()),
 ]
